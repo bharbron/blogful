8c08389f48779933ff406dc040cd9df1a83a7e0c
Brian Harbron <harbron@gmail.com>
2015-06-02 01:33:48 +0000

    Minimal functional posts view

--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,6 @@ from .models import Post
 @app.route("/")
 def posts():
   posts = session.query(Post)
-  posts = posts.order_by(Post.datatime.desc())
+  posts = posts.order_by(Post.datetime.desc())
   posts = posts.all()
-  return render_tempate("posts.html", posts=posts)
+  return render_template("posts.html", posts=posts)
