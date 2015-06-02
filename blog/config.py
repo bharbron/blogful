e72a6423d079387393f34c4ef34d3190fe7815fa
Brian Harbron <harbron@gmail.com>
2015-06-02 01:12:48 +0000

    Added Post model and code for seeding test data

--- blog/config.py
+++ blog/config.py
@@ -1,5 +1,5 @@
 import os
 class DevelopmentConfig(object):
-  SQLALCHEMY_DATABASE_URI = "postgresql://action:action@locathost:5432/blogful"
+  SQLALCHEMY_DATABASE_URI = "postgresql://action:action@localhost:5432/blogful"
   DEBUG = True
   SECRET_KEY = os.environ.get("BLOGFUL_SECRET_KEY", "")
