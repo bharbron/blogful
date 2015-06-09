182eeb2c39d11da87a49b1c33d3eb99ceb79a15f
Brian Harbron <harbron@gmail.com>
2015-06-09 15:53:57 +0000

    Added acceptance test for verifying editing of post as the posts author and that you cant edit if youre not the author

--- tests/test_views_acceptance.py
+++ tests/test_views_acceptance.py
@@ -27,6 +27,18 @@ class TestViews(unittest.TestCase):
     session.add(self.user)
     session.commit()
     
+    # Create a second example user
+    self.user2 = models.User(name="Carlos", email="carlos@example.com", password=generate_password_hash("test"))
+    session.add(self.user2)
+    session.commit()
+    
+    # Create some sample posts
+    content = "A post for acceptance testing"
+    for i in range (5):
+      post = models.Post(title="Acceptance test post #{}".format(i+1), content=content, author=self.user)
+      session.add(post)
+    session.commit()
+        
     self.process = multiprocessing.Process(target=app.run, kwargs={"host": "0.0.0.0", "port": 8080})
     self.process.start()
     time.sleep(1)
@@ -55,6 +67,49 @@ class TestViews(unittest.TestCase):
     button = self.browser.find_by_css("button[type=submit]")
     button.click()
     self.assertEqual(self.browser.url, "http://0.0.0.0:8080/login")
+    
+  def testEditAsAuthor(self):
+    self.browser.visit("http://0.0.0.0:8080/login")
+    self.browser.fill("email", "alice@example.com")
+    self.browser.fill("password", "test")
+    button = self.browser.find_by_css("button[type=submit]")
+    button.click()
+    self.browser.visit("http://0.0.0.0:8080/post/2/edit")
+    self.assertEqual(self.browser.url, "http://0.0.0.0:8080/post/2/edit")
+    self.browser.fill("title", "edited title")
+    self.browser.fill("content", "edited content")
+    button = self.browser.find_by_css("button[type=submit]")
+    button.click()
+    self.assertEqual(self.browser.url, "http://0.0.0.0:8080/post/2")
+    self.assertTrue(self.browser.is_text_present("edited title"))
+    self.assertTrue(self.browser.is_text_present("edited content"))
+  
+  def testEditNotAsAuthor(self):
+    self.browser.visit("http://0.0.0.0:8080/login")
+    self.browser.fill("email", "carlos@example.com")
+    self.browser.fill("password", "test")
+    button = self.browser.find_by_css("button[type=submit]")
+    button.click()
+    self.browser.visit("http://0.0.0.0:8080/post/1/edit")
+    self.assertEqual(self.browser.url, "http://0.0.0.0:8080/")
+  
+  def testDeleteAsAuthor(self):
+    self.browser.visit("http://0.0.0.0:8080/login")
+    self.browser.fill("email", "alice@example.com")
+    self.browser.fill("password", "test")
+    button = self.browser.find_by_css("button[type=submit]")
+    button.click()
+    self.browser.visit("http://0.0.0.0:8080/post/1/delete")
+    self.assertEqual(self.browser.url, "http://0.0.0.0:8080/post/1/delete")
+  
+  def testDeleteNotAsAuthor(self):
+    self.browser.visit("http://0.0.0.0:8080/login")
+    self.browser.fill("email", "carlos@example.com")
+    self.browser.fill("password", "test")
+    button = self.browser.find_by_css("button[type=submit]")
+    button.click()
+    self.browser.visit("http://0.0.0.0:8080/post/1/delete")
+    self.assertEqual(self.browser.url, "http://0.0.0.0:8080/")
     
 if __name__ == "__main__":
   unittest.main()
